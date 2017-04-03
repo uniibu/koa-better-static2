705fc7b48d8a49acfaa88e94847c3780c799cf6e
uniibu <unibtc@gmail.com>
2017-04-03 19:02:37 +0800

    Update for use with Koa@2
    Updated for use with Nodejs 7.x.x and Koa@2.
    Used indexOf instead of for loop.

--- index.js
+++ index.js
@@ -32,50 +32,46 @@ module.exports = serve;
 
 function serve(root, opts) {
   assert(root, 'root directory is required to serve files');
-
-  const options = Object.assign({
+  const options =  Object.assign({
     index: false,
     maxage: 0,
     hidden: false,
     ifModifiedSinceSupport: true
   }, opts);
 
+
   const normalizedRoot = normalize(resolve(root));
 
+
   // options
-  debug('static "%s" %j', root, opts);
+  debug('static "%s" %j', root, options);
 
-  return function serve(ctx, next){   
-
-      if (ctx.method == 'HEAD' || ctx.method == 'GET') {
-
-        const path = ctx.path.substr(parse(ctx.path).root.length);
-        try {
-          path = decodeURIComponent(path);
-        } catch (ex) {
-          ctx.throw('Could not decode path', 400);
-          return;
-        }
-
-        if (options.index && '/' == ctx.path[ctx.path.length - 1])
-          path += options.index;
-
-        path = resolvePath(normalizedRoot, path);
-
-        if (!options.hidden && isHidden(root, path)) return;
-
-        return send(ctx, ctx.path, options).then(done => {
-            if (!done) {
-                return next();
-            }
-        });
+  return function serve(ctx, next){
+    if (ctx.method == 'HEAD' || ctx.method == 'GET') {
+      let path = ctx.path.substr(parse(ctx.path).root.length);
+      try {
+        path = decodeURIComponent(path);
+      } catch (ex) {
+        ctx.throw('Could not decode path', 400);
+        return;
       }
-      return next();   
-  };
-}
+      if (options.index && '/' == ctx.path[ctx.path.length - 1])
+        path += options.index;
+      path = resolvePath(normalizedRoot, path);
+      if (!options.hidden && isHidden(root, path)) return;
 
-// TODO: this can be sped up, with an findIndexOf loop
+      return send(ctx, path, options).then(done => {
+            if (!done) {
+        return next();
+      }
+    });
+    }
+    return next();
+  };
+
+}
+// used indexOf for better speed than for loop https://jsperf.com/js-multiloop
 function isHidden(root, path) {
   path = path.substr(root.length).split(sep);
-  return path.indexOf('.');
+  return (path.indexOf('.') !== -1);
 }
