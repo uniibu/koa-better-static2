8ad92e55db5042c09673dcbd6b61ef5643cdb3b8
uniibu <unibtc@gmail.com>
2017-07-12 22:57:57 +0000

    Update to use async/await

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 'use strict';
-
 /**
  * Module dependencies.
  */
-
-const normalize = require('path').normalize;
-const resolve = require('path').resolve;
-const parse = require('path').parse;
-const sep = require('path').sep;
-
+const debug = require('debug')('koa-better-static2');
+const {normalize, resolve, parse, sep} = require('path');
 const assert = require('assert');
-const debug = require('debug')('koa-better-static');
 const send = require('./send');
 const resolvePath = require('resolve-path');
-
-
-/**
- * Expose `serve()`.
- */
-
-module.exports = serve;
-
 /**
  * Serve static files from `root`.
  *
@@ -29,49 +15,49 @@ module.exports = serve;
  * @return {Function}
  * @api public
  */
-
-function serve(root, opts) {
+const serve = (root, opts) => {
   assert(root, 'root directory is required to serve files');
-  const options =  Object.assign({
+  const options = Object.assign({
     index: false,
     maxage: 0,
     hidden: false,
     ifModifiedSinceSupport: true
   }, opts);
-
-
   const normalizedRoot = normalize(resolve(root));
-
-
-  // options
   debug('static "%s" %j', root, options);
-
-  return function serve(ctx, next){
+  return async (ctx, next) => {
+    let done = false;
     if (ctx.method == 'HEAD' || ctx.method == 'GET') {
       let path = ctx.path.substr(parse(ctx.path).root.length);
       try {
         path = decodeURIComponent(path);
-      } catch (ex) {
+      } catch (e) {
         ctx.throw('Could not decode path', 400);
         return;
       }
       if (options.index && '/' == ctx.path[ctx.path.length - 1])
         path += options.index;
       path = resolvePath(normalizedRoot, path);
-      if (!options.hidden && isHidden(root, path)) return;
-
-      return send(ctx, path, options).then(done => {
-            if (!done) {
-        return next();
+      if (!options.hidden && isHidden(root, path))
+        return;
+      try {
+        done = await send(ctx, path, options);
+      } catch (err) {
+        if (err.status !== 404) {
+          throw err;
+        }
       }
-    });
     }
-    return next();
+    if (!done) {
+      await next();
+    }
   };
-
-}
-// used indexOf for better speed than for loop https://jsperf.com/js-multiloop
+};
 function isHidden(root, path) {
   path = path.substr(root.length).split(sep);
-  return (path.indexOf('.') !== -1);
+  return path.includes('.');
 }
+/**
+ * Expose `serve()`.
+ */
+module.exports = serve;
--- send.js
+++ send.js
@@ -1,18 +1,16 @@
 /**
  * Module dependencies.
  */
-
-const debug = require('debug')('koa-better-static:send');
+const debug = require('debug')('koa-better-static2:send');
 const assert = require('assert');
-const extname = require('path').extname;
-const fs = require('mz/fs');
-
+const {extname} = require('path');
+const {promisify} = require('util');
+const fs = require('fs');
+const fsStat = promisify(fs.stat);
 /**
  * Expose `send()`.
  */
-
 module.exports = send;
-
 /**
  * Send file at `path` with the
  * given `options` to the koa `ctx`.
@@ -24,60 +22,50 @@ module.exports = send;
  * @return {Function}
  * @api public
  */
-
 async function send(ctx, path, opts) {
     assert(ctx, 'koa context required');
     assert(path, 'pathname required');
     assert(opts, 'opts required');
-
     // options
     debug('send "%s" %j', path, opts);
     const index = opts.index;
     const maxage = opts.maxage;
     const format = opts.format;
     const ifModifiedSinceSupport = opts.ifModifiedSinceSupport;
-
-    // stat
-    let stats;
     try {
-      stats = await fs.stat(path);
-    // Format the path to serve static file servers
-    // and not require a trailing slash for directories,
-    // so that you can do both `/directory` and `/directory/`
-    if (stats.isDirectory()) {
-      if (format && index) {
-        path += '/' + index;
-        stats = await fs.stat(path);
-      } else {
-        return;
-      }
-    }
-
+        let stats = await fsStat(path);
+        // Format the path to serve static file servers
+        // and not require a trailing slash for directories,
+        // so that you can do both `/directory` and `/directory/`
+        if (stats.isDirectory()) {
+            if (format && index) {
+                path += `/${ index }`;
+                stats = await fsStat(path);
+            } else {
+                return;
+            }
+        }
     } catch (err) {
-      const notfound = ['ENOENT', 'ENAMETOOLONG', 'ENOTDIR'];
-      if (~notfound.indexOf(err.code)) return;
-      err.status = 500;
-      throw err;
+        const notfound = ['ENOENT', 'ENAMETOOLONG', 'ENOTDIR'];
+        if (~notfound.indexOf(err.code))
+            return;
+        err.status = 500;
+        throw err;
     }
-
- 
-    ctx.set('Cache-Control', 'max-age=' + (maxage / 1000 | 0));
-
+    ctx.set('Cache-Control', `max-age=${ maxage / 1000 | 0 }`);
     // Check if we can return a cache hit
     if (ifModifiedSinceSupport) {
-        const ims = Math.floor(Date.parse(ctx.get('If-Modified-Since'))/1000);
-        const mtime = Math.floor(stats.mtime.getTime()/1000);
-        if(ims && ims === mtime){
+        const ims = Math.floor(Date.parse(ctx.get('If-Modified-Since')) / 1000);
+        const mtime = Math.floor(stats.mtime.getTime() / 1000);
+        if (ims && ims === mtime) {
             ctx.status = 304;
             return path;
         }
     }
-
     // stream
     ctx.set('Last-Modified', stats.mtime.toUTCString());
     ctx.set('Content-Length', stats.size);
     ctx.type = extname(path);
     ctx.body = fs.createReadStream(path);
-
     return path;
 }
